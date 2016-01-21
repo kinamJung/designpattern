7215be1665ef803e38e00e85c623234933d49234
정기남 <jkn5202@naver.com>
2016-01-21 14:14:11 +0900

    branch test

--- src/com/hanains/bridge/BridegeTest.java
+++ src/com/hanains/bridge/BridegeTest.java
@@ -6,10 +6,11 @@ package com.hanains.bridge;
  *   - 구현부에서 추상층을 분리하여 각자 독립적으로 변형할 수 있게 하는 패턴이다.
  *     즉, 기능과 구현을 별도의 클래스로 정의해서 서로를 분리하는 방법이다.
  * 	 - 참고. Adapter Pattern
+ * 
  * */
 
 public class BridegeTest {
-	
+	  
 	public static void main(String[] args) {
 		ConcreteImplementorA impl = new ConcreteImplementorA();
 		ConcreteImplementorB impl2 = new ConcreteImplementorB();
